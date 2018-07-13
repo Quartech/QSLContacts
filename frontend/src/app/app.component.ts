4cd9a319186154ba7a54d6aa22fe1e9fa40b6312
Jeremy Dunsdon <twenty15creative@gmail.com>
2018-07-13 13:25:54 -0700

    [JD-2] Updating the project to support SCSS

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { ApiService } from '@app/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'QSLContacts';
