beeb8e4f274d8b18fa96ef737ddbfd1db22a2d52
Nick Phura <Nick.Phura@quartech.com>
2018-07-13 15:42:37 -0700

    Fixed unit tests. Minor updates to docker files. Added .prettierrc

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,11 +1,15 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { ContactListComponent } from './components/contact-list/contact-list.component';
+import { ApiService } from '@app/core';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
+      providers: [
+        { provide: ApiService, useValue: jasmine.createSpy('ApiService') }
       ],
+      declarations: [AppComponent, ContactListComponent]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -13,15 +17,9 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'frontend'`, async(() => {
+  it(`should have as title 'QSLContacts'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('frontend');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to frontend!');
+    expect(app.title).toEqual('QSLContacts');
   }));
 });
--- frontend/src/app/core/http/api.service.spec.ts
+++ frontend/src/app/core/http/api.service.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { ApiService } from './api.service';
 
 describe('ApiService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ApiService]
+      providers: [ApiService],
+      imports: [HttpClientTestingModule]
     });
   });
 
